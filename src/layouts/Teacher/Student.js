b078a32c364574009d9e5584635527288d4a845c
Syed Muhamad Danial <syed.danial@wallet-codes.com>
2024-01-08 00:47:57 +0800

    wrapped <Student /> in Card

--- src/layouts/Teacher/Student.js
+++ src/layouts/Teacher/Student.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 import { getRequest } from "../../helpers/apiHandlers";
+
+/* common */
+import WrapperCard from "../../components/shared/Cards/WrapperCard";
 
 /* components */
 import StudentTable from "../../components/Teacher/Student/StudentTable";
@@ -61,20 +65,32 @@ class Student extends Component {
     const { isLoading, students } = this.state;
 
     return (
-      <div className="homework">
-        <StudentTable
-          data={{ isLoading, students }}
-          handleAction={(e, id) => this.handleAction(e, id)}
-          handleShowAddStudentModal={this.handleShowAddStudentModal}
-        />
-        {this.state.showAddStudentModal && (
-          <AddStudentModal
-            isLoading={isLoading}
-            showAddStudentModal={this.state.showAddStudentModal}
-            handleShowAddStudentModal={this.handleShowAddStudentModal}
-            handleSaveAddStudent={this.handleSaveAddStudent}
-          />
-        )}
+      <div className="student">
+        <div className="row">
+          <div className="col-12">
+            <WrapperCard
+              header={{
+                exist: false,
+              }}
+            >
+              <Card.Body>
+                <StudentTable
+                  data={{ isLoading, students }}
+                  handleAction={(e, id) => this.handleAction(e, id)}
+                  handleShowAddStudentModal={this.handleShowAddStudentModal}
+                />
+                {this.state.showAddStudentModal && (
+                  <AddStudentModal
+                    isLoading={isLoading}
+                    showAddStudentModal={this.state.showAddStudentModal}
+                    handleShowAddStudentModal={this.handleShowAddStudentModal}
+                    handleSaveAddStudent={this.handleSaveAddStudent}
+                  />
+                )}
+              </Card.Body>
+            </WrapperCard>
+          </div>
+        </div>
       </div>
     );
   }
